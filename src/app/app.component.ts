f55c786dc747f5bd3d3838757db04166baf976a3
Mimy-chan <mimy291628@gmail.com>
2020-03-19 22:57:47 +0100

    creazione menu base,molto semplice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PortaleVideogiochi';
+  title = 'Games World';
+  currentSection:number=1;
 }
--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/interface/menu-item.interface';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @Output()
+selectMenuId: EventEmitter<number> = new EventEmitter();
+
+  menuItems: MenuItem[]=[
+    {
+      id: 1, description: 'Home', selected: true
+    },
+    {
+      id: 2, description: 'Lista Videogiochi', selected: false
+    },
+    {
+      
+      id: 3, description: 'Modifica', selected: false
+  
+  }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  selectMenuItem(id: number) {
+    this.selectMenuId.emit(id);
 
+  }
 }
