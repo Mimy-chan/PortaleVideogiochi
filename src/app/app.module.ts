55eb344cfa6582b583719397f69381a7bae9d651
Mimy-chan <mimy291628@gmail.com>
2020-03-20 12:18:32 +0100

    configurazione routing e semplicissimo menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { HomeComponent } from './component/home/home.component';
 import { GamesListComponent } from './component/games-list/games-list.component';
 import { GameDetailComponent } from './component/game-detail/game-detail.component';
 import { EditGameComponent } from './component/edit-game/edit-game.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'gamesList', component: GamesListComponent },
+  { path: 'gameDetail', component: GameDetailComponent },
+  { path: 'editGame', component: EditGameComponent }
+];
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +29,9 @@ import { EditGameComponent } from './component/edit-game/edit-game.component';
     EditGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
