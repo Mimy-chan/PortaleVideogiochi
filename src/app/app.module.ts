0d36e727f42a7290b456ba8394467b191912f267
Mimy-chan <mimy291628@gmail.com>
2020-03-20 18:01:17 +0100

    ultime modifiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { HomeComponent } from './component/home/home.component';
 import { GamesListComponent } from './component/games-list/games-list.component';
 import { GameDetailComponent } from './component/game-detail/game-detail.component';
 import { EditGameComponent } from './component/edit-game/edit-game.component';
-import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'gamesList', component: GamesListComponent },
+  { path: 'editGame', component: EditGameComponent },
   { path: 'gameDetail', component: GameDetailComponent },
-  { path: 'editGame', component: EditGameComponent }
+  { path: 'login', component: LoginComponent },
 ];
 
 
@@ -26,11 +28,13 @@ const appRoutes: Routes = [
     HomeComponent,
     GamesListComponent,
     GameDetailComponent,
-    EditGameComponent
+    EditGameComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
 
   ],
   providers: [],
